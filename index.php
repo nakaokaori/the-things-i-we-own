2d21389685102be4d14e22e5e6d8a62c9e66c982
nakaokaori <108987557+nakaokaori@users.noreply.github.com>
2022-08-05 12:48:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ fclose($fp);
     <link rel="stylesheet" href="images.css" />
     <style>
         :root {
-            --text-color: navy;
+            --text-color: seashell;
             --hover-color: #5df;
             --border-style: 1px dashed #4c8;
             --bg-color: #d41;
